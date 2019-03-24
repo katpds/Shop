195944c1aa1c44a800d47c67b319b808760c6180
Katherine Paladines <katdpds@gmail.com>
2019-03-23 23:16:27 -0500

    Consume Token From App. Add Models in the Shop Common Models: Token Request, Token Response. Modify the ApiServices Class, LoginViewModel, MainViewModel, LoginPage View, App View

---
+++ Shop.Common/Models/TokenRequest.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace Shop.Common.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    public class TokenRequest
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+
+}
--- Shop.UIForms/Shop.UIForms/ViewModels/LoginViewModel.cs
+++ Shop.UIForms/Shop.UIForms/ViewModels/LoginViewModel.cs
@@ -1,62 +1,92 @@
-﻿
-
-namespace Shop.UIForms.ViewModels
+﻿namespace Shop.UIForms.ViewModels
 {
     using System;
     using System.Windows.Input;
     using GalaSoft.MvvmLight.Command;
+    using Shop.Common.Models;
+    using Shop.Common.Services;
     using Shop.UIForms.Views;
     using Xamarin.Forms;
 
-    public class LoginViewModel 
+     public class LoginViewModel: BaseViewModel
+{
+	private bool isRunning;
+    private bool isEnabled;
+    private readonly ApiService apiService; 
+
+    public bool IsRunning
     {
-        public string Email { get; set; }
-
-        public string Password { get; set; }
-
-        public ICommand LogicCommand => new RelayCommand(Login);
-
-        public LoginViewModel()
-        {
-            this.Email = "katdpds@gmail.com";
-            this.Password = "123456";
-        }
-
-        private async void Login()
-        {
-            if (string.IsNullOrEmpty(this.Email))
-            {
-                await Application.Current.MainPage.DisplayAlert("Error", "You must enter an email", "Accept");
-                return;
-            }
-
-            if (string.IsNullOrEmpty(this.Password))
-            {
-                await Application.Current.MainPage.DisplayAlert(
-                    "Error", 
-                    "You must enter a password", 
-                    "Accept");
-                return;
-            }
-
-            if (!this.Email.Equals("katdpds@gmail.com") || !this.Password.Equals("123456"))
-            {
-                await Application.Current.MainPage.DisplayAlert(
-                    "Error", 
-                    "Incorrect user or password", 
-                    "Accept");
-                return;
-            }
-
-            //await Application.Current.MainPage.DisplayAlert(
-            //    "Ok", 
-            //    "Fuck yeah!!!", 
-            //    "Accept");
-
-
-            MainViewModel.GetInstance().Products = new ProductsViewModel();
-            await Application.Current.MainPage.Navigation.PushAsync(new ProductsPage());
-        }
-
+        get => this.isRunning;
+        set => this.SetValue(ref this.isRunning, value);
     }
+
+    public bool IsEnabled
+    {
+        get => this.isEnabled;
+        set => this.SetValue(ref this.isEnabled, value);
+    }
+
+    public string Email { get; set; }
+
+    public string Password { get; set; }
+
+    public ICommand LoginCommand => new RelayCommand(this.Login);
+
+    public LoginViewModel()
+    {
+        this.apiService = new ApiService();
+        this.IsEnabled = true;
+        this.Email = "katdpds@gmail.com";
+        this.Password = "123456";
+    }
+
+    private async void Login()
+    {
+        if (string.IsNullOrEmpty(this.Email))
+        {
+            await Application.Current.MainPage.DisplayAlert("Error", "You must enter an email.", "Accept");
+            return;
+        }
+
+        if (string.IsNullOrEmpty(this.Password))
+        {
+            await Application.Current.MainPage.DisplayAlert("Error", "You must enter a password.", "Accept");
+            return;
+        }
+
+        this.IsRunning = true;
+        this.IsEnabled = false;
+
+        var request = new TokenRequest
+        {
+            Password = this.Password,
+            Username = this.Email
+        };
+
+        var url = Application.Current.Resources["UrlAPI"].ToString();
+        var response = await this.apiService.GetTokenAsync(
+            url,
+            "/Account",
+            "/CreateToken",
+            request);
+
+        this.IsRunning = false;
+        this.IsEnabled = true;
+
+        if (!response.IsSuccess)
+        {
+            await Application.Current.MainPage.DisplayAlert("Error", 
+                "Email or password incorrect.", 
+                "Accept");
+            return;
+        }
+
+        var token = (TokenResponse)response.Result;
+        var mainViewModel = MainViewModel.GetInstance();
+        mainViewModel.Token = token;
+        mainViewModel.Products = new ProductsViewModel();
+        await Application.Current.MainPage.Navigation.PushAsync(new ProductsPage());
+    }
+}
+
 }
